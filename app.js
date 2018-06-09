f567a7aba08c40eb763d3fe6be9cb2ae4f28f7ae
Trym Vegard Gjelseth-Borgen <trym@Tryms-MacBook-Air.local>
2018-06-09 12:03:17 +0200

    First commit for RFGK

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/static_html/index.html'));
+});
+
+app.get('/Volleydisc', (req, res, next) => {
+    res.sendFile(path.join(__dirname + '/public/static_html/Volleydisc.html'));
+});
+
+app.use('/html', router);
+
+module.exports = app;
