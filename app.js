6b3dd09c635c6348efa73d5f205ec4ab28e53251
Trym Vegard Gjelseth-Borgen <trym@Tryms-MacBook-Air.local>
2018-06-09 15:13:02 +0200

    Volleydisc page finished

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const router = express.Router();
+
+const app = express();
 
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function(req, res) {
-    console.log("Someone visited");
     res.sendFile(path.join(__dirname + '/public/static_html/index.html'));
 });
 
@@ -16,6 +15,5 @@ app.get('/Volleydisc', (req, res, next) => {
     res.sendFile(path.join(__dirname + '/public/static_html/Volleydisc.html'));
 });
 
-app.use('/html', router);
 
 module.exports = app;
