78cad495c304e393848677c1c791dc4f0bb8e487
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2019-04-08 13:57:14 +0200

    Test for SSL certificate

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/xn--rnvikfrisbeegolf-lxb.no/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/xn--rnvikfrisbeegolf-lxb.no/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/xn--rnvikfrisbeegolf-lxb.no/chain.pem', 'utf8');
 
-const server = http.createServer(app);
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+const portHTTP = process.env.PORT || 3000;
+const portHTTPS = 443;
 
 server.listen(port, () => {
     console.log("Server listening on", port);
+});
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(portHTTP, () => {
+	console.log('HTTP Server running on port', portHTTP);
+});
+
+httpsServer.listen(portHTTPS, () => {
+	console.log('HTTPS Server running on port 443', portHTTPS);
 });
