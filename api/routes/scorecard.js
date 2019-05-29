66a1b9fbb971d639c3daa2ce51b3bcd60c11573b
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2019-05-30 01:11:19 +0200

    Finished rework for scorecard route

--- api/routes/scorecard.js
+++ api/routes/scorecard.js
@@ -6,6 +6,7 @@ const Round = require('../../src/models/Round');
 const Player = require('../../src/models/Player');
 const Course = require('../../src/models/Course');
 
+// Returns all existing scorecards.
 router.get('/', (req, res, next) =>  {
 
 	Scorecard.findAll({
@@ -29,45 +30,46 @@ router.get('/', (req, res, next) =>  {
 		order: [['date_time', 'DESC']]
 	}).then(scorecards => {
 		res.json(scorecards);
-	})
-
-    // const query =  `SELECT Scorecard.id, Scorecard.date_time, Player.first_name, Player.last_name
-	// 				FROM Scorecard
-	// 					INNER JOIN Player ON Scorecard.created_by = Player.id
-	// 				ORDER BY UNIX_TIMESTAMP(date_time) DESC;`;
-		
-    // pool.query(query, (err, rows, fields) => {
-
-    //     if (err) {
-    //         console.log("Failed getting all scorecards: " + err);
-	// 		res.sendStatus(500);
-    //         return;
-    //     }
-
-    //     res.json(rows);
-    // });
+	}).catch(function (err) {
+		console.log(`Failed getting all scorecards: ${err}`);
+		res.sendStatus(500);
+		return;
+	});
 });
 
+
+// Returns scorecard with id "scorecard_id" if it exists.
 router.get('/:scorecard_id', (req, res, next) =>  {
-    const scorecard_id = req.params.scorecard_id;
-    const query =  `SELECT Player.first_name, Player.last_name, Course.name, Course.par, Round.number_of_throws, Round.date
-					FROM Scorecard
-					INNER JOIN Round on Scorecard.id = Round.scorecard_id
-					INNER JOIN Course on Round.course_id = Course.id
-					INNER JOIN Player on Round.player_id = Player.id
-					WHERE scorecard_id = ?
-					ORDER BY number_of_throws;`;
+	const scorecard_id = req.params.scorecard_id;
+	
+	Scorecard.findOne ({
+		attributes: ['date_time', 'id'],
+		include: [{
+			model: Round,
+				attributes: [['number_of_throws', 'throws'], 'date'],
+				include: [{
+					model: Player,
+					attributes: ['first_name', 'last_name']
+				},
+				{
+					model: Course,
+					attributes: [['name', 'course_name'], 'holes', 'par']
+				}]
 
-    pool.query(query, [scorecard_id], (err, rows, fields) => {
-
-        if (err) {
-            console.log("Failed getting all scorecards: " + err);
-            res.sendStatus(500);
-            return;
-        }
-
-        res.json(rows);
-    });
+		},{
+			model: Player,
+			attributes: ['first_name', 'last_name']
+		}],
+		where: {
+			id: scorecard_id
+		}
+	}).then(scorecards => {
+		res.json(scorecards);
+	}).catch(function (err) {
+		console.log(`Failed getting scorecard with id ${scorecard_id}: ${err}`);
+		res.sendStatus(500);
+		return;
+	});
 });
 
 module.exports = router;
