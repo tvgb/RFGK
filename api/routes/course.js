3c9f816cd338dfc882272aa671994aa0b830a32e
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2019-05-31 12:37:16 +0200

    Made functionable orm backend for Scorecards

--- api/routes/course.js
+++ api/routes/course.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../database');
-const config = require('../../config');
 const Course = require('../../src/models/Course');
 
 
-
+// Get all Courses
 router.get('/', (req, res, next) =>  {
 
 	Course.findAll()
@@ -13,38 +11,27 @@ router.get('/', (req, res, next) =>  {
 		res.json(courses);
 	})
 	.catch(err => {
-		console.log("Failed getting all courses: " + err);
+		console.log(`Failed getting all courses: ${err}`);
 		res.sendStatus(500);
 		return;
 	});
-
-
-    // const query = "SELECT * FROM Course";
-
-    // pool.query(query, (err, rows, fields) => {
-    //     if (err) {
-    //         console.log("Failed getting all courses: " + err);
-    //         res.sendStatus(500);
-    //         return;
-    //     }
-
-    //     res.json(rows);
-    // });
 });
 
+// Get course with id "course_id"
 router.get('/:course_id', (req, res, next) =>  {
-    const course_id = req.params.course_id;
-    const query = "SELECT * FROM Course WHERE id = ?";
-
-    pool.query(query, [course_id], (err, rows, fields) => {
-        if (err) {
-            console.log("Failed getting a course: " + err);
-            res.sendStatus(500);
-            return;
-        }
-
-        res.json(rows);
-    });
+	const course_id = req.params.course_id;
+	
+	Course.findOne({
+		where: {
+			id: course_id
+		}
+	}).then(course => {
+		res.json(course);
+	}).catch(err => {
+		console.log(`Failed getting course with id ${course_id}: ${err}`);
+		res.sendStatus(500);
+		return;
+	})
 });
 
 module.exports = router;
--- api/routes/players.js
+++ api/routes/players.js
@@ -6,48 +6,56 @@ const jwt = require('jsonwebtoken');
 const checkAuth = require('../middleware/check-auth');
 const Player = require('../../src/models/Player');
 const Scorecard = require('../../src/models/Scorecard');
+const Round = require('../../src/models/Round');
+const sequalize = require('sequelize');
 
-
+// Get all players
 router.get('/', (req, res, next) =>  {
-	const query = 'SELECT * FROM Player;';
-	
-    pool.query(query, (err, rows, fields) => {
-        if (err) {
-            console.log("Failed getting all players: " + err)
-            res.sendStatus(500);
-            return;
-        }
 
-        res.json(rows);
-    });
+	Player.findAll({
+
+	}).then(players => {
+		res.json(players);
+	}).catch(err => {
+		console.log(`Failed getting all players: ${err}`);
+		res.sendStatus(500);
+		return;
+	})
 });
 
-router.get('/test', (req, res, next) => {
-
-	// search for attributes
-	Player.findAll({ include: [{
-		model: Scorecard,
-		as: 'Scorecard'
-	}]}).then(player => {
-		// project will be the first entry of the Projects table with the title 'aProject' || null
+// Get player with id "player_id"
+router.get('/:player_id', (req, res, next) => {
+	const player_id = req.params.player_id;
+	
+	Player.findOne({
+		where: {
+			id: player_id
+		}
+	}).then(player => {
 		res.json(player);
+	}).catch(err => {
+		console.log(`Failed getting player with id ${player_id}: ${err}`);
+		res.sendStatus(500);
+		return;
 	})
-})
+});
 
+router.get('/with_rounds/:course_id/:year', (req, res, next) => {
+	const course_id = req.params.course_id;
+	const year = req.params.year;
 
-router.get('/:id', (req, res, next) => {
-    const player_id = req.params.id;
-    const query = 'SELECT * FROM Player WHERE id = ?;';
-
-    pool.query(query, [player_id], (err, rows, fields) => {
-        if (err) {
-            console.log("Failed getting player by id: " + err);
-            res.sendStatus(500);
-            return;
-        }
-
-        res.json(rows);
-    });
+	Player.findAll({
+		include: [{
+			model: Round,
+			where: sequalize.where(sequalize.fn('YEAR', sequalize.col('date')), '=', year)
+		}]
+	}).then(players => {
+		res.json(players);
+	}).catch(err => {
+		console.log(`Failed getting players with rounds: ${err}`);
+		res.sendStatus(500);
+		return;
+	});
 });
 
 router.get('/leaderboard/avg/:course_id', (req, res, next) => {
@@ -72,7 +80,8 @@ router.get('/leaderboard/avg/:course_id', (req, res, next) => {
 });
 
 router.get('/leaderboard/best/:course_id', (req, res, next) => {
-    const course_id = req.params.course_id;
+	const course_id = req.params.course_id;
+
     const query =  `SELECT Player.first_name, Player.last_name, Course.name, Course.par,
         			DATE_FORMAT(MAX(Round.date), '%d-%m-%Y') AS date, MIN(Round.number_of_throws) AS throws
         			FROM Round
@@ -96,64 +105,64 @@ router.get('/leaderboard/best/:course_id', (req, res, next) => {
 
 
 router.post('/signup', (req, res, next) => {
-   if (process.env.SECRET_CODE === req.body.secret_code) {
-
-	   const query = "SELECT * FROM Player WHERE email = ?;";
+   	if (process.env.SECRET_CODE === req.body.secret_code) {
+	
+		const query = "SELECT * FROM Player WHERE email = ?;";
 	   
-       pool.query(query, [req.body.email], (err, rows, fields) => {
-           if (err) {
-               return res.status(500).json({
-                   message: 'Internal server error'
-               })
-           }
+       	pool.query(query, [req.body.email], (err, rows, fields) => {
+           	if (err) {
+               	return res.status(500).json({
+                   	message: 'Internal server error'
+               	})
+           	}
 
-           if (rows.length > 0) {
-               return res.status(401).json({
-                   message: 'Auth failed'
-               })
-           } else {
-               bcrypt.hash(req.body.password, 10, (error, hash) => {
-                   if (error) {
-                       return res.status(500).json({
-                           error: error
-                       })
-                   } else {
-                       const user = {
-                           first_name: req.body.first_name,
-                           last_name: req.body.last_name,
-                           email: req.body.email,
-                           password: hash,
-                           birthday: req.body.birthday
-                       };
+           	if (rows.length > 0) {
+            	return res.status(401).json({
+                   	message: 'Auth failed'
+               	})
+           	} else {
+               	bcrypt.hash(req.body.password, 10, (error, hash) => {
+                   	if (error) {
+                       	return res.status(500).json({
+                           	error: error
+                       	})
+                   	} else {
+                       	const user = {
+                           	first_name: req.body.first_name,
+                           	last_name: req.body.last_name,
+                           	email: req.body.email,
+                           	password: hash,
+                           	birthday: req.body.birthday
+                       	};
 
-                       const connection = getConnection();
-                       const query = "INSERT INTO Player(first_name, last_name, email, password, birthday) VALUES (?, ?, ?, ?, ?);";
-                       connection.query(query, [user.first_name, user.last_name, user.email, user.password, user.birthday], (error2, rows2, fields) => {
-                           if (error2) {
-                               console.log("Failed to create new user: " + error2);
-                               res.sendStatus(500);
-                               return;
-                           }
+                       	const connection = getConnection();
+                       	const query = "INSERT INTO Player(first_name, last_name, email, password, birthday) VALUES (?, ?, ?, ?, ?);";
+                       	connection.query(query, [user.first_name, user.last_name, user.email, user.password, user.birthday], (error2, rows2, fields) => {
+                           	if (error2) {
+                               	console.log("Failed to create new user: " + error2);
+                               	res.sendStatus(500);
+                               	return;
+                           	}
 
-                           return res.status(200).json({
-                               message: 'User succesfully created'
-                           })
-                       });
-                   }
-               });
-           }
-       });
+                           	return res.status(200).json({
+                               	message: 'User succesfully created'
+                           	})
+                       	});
+                   	}
+               	});
+           	}
+       	});
 
-   } else {
-       return res.status(401).json({
-           message: 'wrong secret code'
-       })
-   }
+   	} else {
+       	return res.status(401).json({
+           	message: 'wrong secret code'
+       	});
+   	}
 });
 
 router.post('/login', (req, res, next) => {
 
-   const query = "SELECT id, password FROM Player WHERE email = ?;";
+   	const query = "SELECT id, password FROM Player WHERE email = ?;";
 
 	pool.query(query, [req.body.email], (err, rows, fields) => {
 		if (err) {
